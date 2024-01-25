7ce119d287251fdfaa26d888ee3d0e6578943c80
Felix Kwong <felixkwong068@FelixdeMacBook-Pro-3.local>
2024-01-24 19:38:04 -0600

    initial commit

---
+++ R/mysquare.R
@@ -0,0 +1,12 @@
+#' Basic square function
+#'
+#' @param x a vector of quantitative data
+#'
+#' @return the square of the input vector
+#' @export
+#'
+#' @examples
+#' mysquare(x=1:20)
+mysquare<-function(x){
+  x^2
+}
